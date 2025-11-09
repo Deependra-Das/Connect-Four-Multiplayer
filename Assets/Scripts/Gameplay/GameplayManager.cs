49f0ee128a7cf36777bd79a52203d4866dc8cf6c
Deependra-Das <iamdeep75@gmail.com>
2025-11-10 02:41:56 +0530

    Changes to handle the board state based on the player turn

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -4,20 +4,34 @@ public class GameplayManager : GenericMonoSingleton<GameplayManager>
 {
     [SerializeField] private Transform[] _spawnLocations;
 
-    bool playerTurn = false;
+    PlayerTurnEnum playerTurn = PlayerTurnEnum.None;
+
+    public void Initialize()
+    {
+        GameManager.Instance.Get<BoardService>().InitializeBoard();
+        playerTurn = PlayerTurnEnum.Player1;
+    }
 
     public void TakeTurn(int colIndex)
     {
-        switch (playerTurn)
-        {
-            case false:
-                GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskRed, _spawnLocations[colIndex].position);
-                break;
-            case true:
-                GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskYellow, _spawnLocations[colIndex].position);
-                break;
-        }
+        if (UpdateBoardState(colIndex, (int)playerTurn))
+            {
+            switch (playerTurn)
+            {
+                case PlayerTurnEnum.Player1:
+                    GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskRed, _spawnLocations[colIndex].position);
+                    playerTurn = PlayerTurnEnum.Player2;
+                    break;
+                case PlayerTurnEnum.Player2:
+                    GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskYellow, _spawnLocations[colIndex].position);
+                    playerTurn = PlayerTurnEnum.Player1;
+                    break;
+            }
+        }        
+    }
 
-        playerTurn = !playerTurn;
+    private bool UpdateBoardState(int col, int value)
+    {
+        return GameManager.Instance.Get<BoardService>().SetBoardCellValue(col, value);
     }
 }
--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GameManager : GenericMonoSingleton<GameManager>
 {
     [SerializeField] private DiskScriptableObject _disk_SO;
+    [SerializeField] private BoardScriptableObject _board_SO;
 
     protected override void Awake()
     {
@@ -12,11 +13,13 @@ public class GameManager : GenericMonoSingleton<GameManager>
     private void Start()
     {
         RegisterServices();
+        GameplayManager.Instance.Initialize();
     }
 
     private void RegisterServices()
     {
         ServiceLocator.Register(new DiskSpawnService(_disk_SO));
+        ServiceLocator.Register(new BoardService(_board_SO));
     }
 
     private void DeregisterServices()
