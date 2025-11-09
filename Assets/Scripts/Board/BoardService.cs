ca58905796f8451c26217faafc05063783bad593
Deependra-Das <iamdeep75@gmail.com>
2025-11-10 02:40:23 +0530

    Added BoardService & Board Scriptable Object

---
+++ Assets/Scripts/Board/BoardService.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+public class BoardService 
+{
+    private int[,] _board;
+    private int _boardRowCount;
+    private int _boardColumnCount;
+
+    public BoardService(BoardScriptableObject boardScriptableObject)
+    {
+        _boardRowCount = boardScriptableObject.boardRowCount;
+        _boardColumnCount = boardScriptableObject.boardColumnCount;
+    }
+
+    ~BoardService()
+    {
+        _board = null;
+    }
+
+    public void InitializeBoard()
+    {
+        _board = new int[_boardRowCount, _boardColumnCount];
+    }
+
+    public bool SetBoardCellValue(int col, int value)
+    {
+        for(int row= _boardRowCount-1; row >= 0 ; row--)
+        {
+            if (_board[row, col] == 0)
+            {
+                _board[row, col] = value;
+                PrintBoardLog();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void PrintBoardLog()
+    {
+        int rows = _board.GetLength(0);
+        int cols = _board.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            string rowValues = "";
+            for (int j = 0; j < cols; j++)
+            {
+                rowValues += _board[i, j] + " ";
+            }
+            Debug.Log(rowValues);
+        }
+    }
+
+}
