38e86bb039410b82ed120cdbb9b1bbcc72c2fa24
Deependra-Das <iamdeep75@gmail.com>
2025-11-10 21:47:46 +0530

    Changes to highlight Winning criteria Disks

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -3,6 +3,8 @@ using ConnectFourMultiplayer.Disk;
 using ConnectFourMultiplayer.Event;
 using ConnectFourMultiplayer.Main;
 using ConnectFourMultiplayer.Utilities;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ConnectFourMultiplayer.Gameplay
@@ -10,12 +12,19 @@ namespace ConnectFourMultiplayer.Gameplay
     public class GameplayManager : GenericMonoSingleton<GameplayManager>
     {
         [SerializeField] private Transform[] _spawnLocations;
+        [SerializeField] private GameObject _highlightPrefab;
 
         private PlayerTurnEnum _playerTurn = PlayerTurnEnum.None;
+        private GameObject[,] spawnedDisks;
+        private int _rowCount = 0;
+        private int _colCount = 0;
 
         public void Initialize()
         {
             GameManager.Instance.Get<BoardService>().InitializeBoard();
+            _rowCount = GameManager.Instance.Get<BoardService>().RowCount;
+            _colCount = GameManager.Instance.Get<BoardService>().ColumnCount;
+            InitializeSpawnedDiskMatrix(_rowCount, _colCount);
             GameManager.Instance.Get<DiskPreviewService>().Initialize(_spawnLocations[0].position);
             _playerTurn = PlayerTurnEnum.Player1;
         }
@@ -25,18 +34,21 @@ namespace ConnectFourMultiplayer.Gameplay
             if (UpdateBoardState(colIndex, (int)_playerTurn))
             {
                 EventBusManager.Instance.Raise(EventNameEnum.TakeTurn, _playerTurn);
+                GameObject newDiskSpawned= null;
 
                 switch (_playerTurn)
                 {
                     case PlayerTurnEnum.Player1:
-                        GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskRed, _spawnLocations[colIndex].position);
+                        newDiskSpawned = GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskRed, _spawnLocations[colIndex].position);
                         break;
                     case PlayerTurnEnum.Player2:
-                        GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskYellow, _spawnLocations[colIndex].position);
+                        newDiskSpawned= GameManager.Instance.Get<DiskSpawnService>().SpawnDisk(DiskTypeEnum.DiskYellow, _spawnLocations[colIndex].position);
                         break;
                 }
-
+                
                 int rowIndex = GameManager.Instance.Get<BoardService>().LastAddedCellRow;
+                AddSpawnedDiskInMatrix(newDiskSpawned, rowIndex, colIndex);
+
                 if (CheckForWin(rowIndex, colIndex))
                 {
                     HandleWin();
@@ -45,6 +57,11 @@ namespace ConnectFourMultiplayer.Gameplay
 
                 _playerTurn = (_playerTurn == PlayerTurnEnum.Player1) ? PlayerTurnEnum.Player2 : PlayerTurnEnum.Player1;
             }
+        }
+
+        private void AddSpawnedDiskInMatrix(GameObject newDiskSpawned, int rowIndex, int colIndex)
+        {
+            spawnedDisks[rowIndex, colIndex] = newDiskSpawned;
         }
 
         private bool UpdateBoardState(int col, int value)
@@ -60,36 +77,45 @@ namespace ConnectFourMultiplayer.Gameplay
         private bool CheckForWin(int rowIndex, int colIndex)
         {
             int playerValue = (int)_playerTurn;
+            List<Vector2Int> winningDiscs = new List<Vector2Int>();
 
-            return CheckDirection(rowIndex, colIndex, 0, 1, playerValue) ||
-                   CheckDirection(rowIndex, colIndex, 1, 0, playerValue) ||
-                   CheckDirection(rowIndex, colIndex, 1, 1, playerValue) ||
-                   CheckDirection(rowIndex, colIndex, 1, -1, playerValue);
+            bool win = CheckDirection(rowIndex, colIndex, 0, 1, playerValue, winningDiscs) ||
+                       CheckDirection(rowIndex, colIndex, 1, 0, playerValue, winningDiscs) ||
+                       CheckDirection(rowIndex, colIndex, 1, 1, playerValue, winningDiscs) ||
+                       CheckDirection(rowIndex, colIndex, 1, -1, playerValue, winningDiscs);
+
+            if (win)
+            {
+                winningDiscs.Add(new Vector2Int(rowIndex, colIndex));
+                StartCoroutine(HighlightWinningDiscs(winningDiscs));
+            }
+
+            return win;
         }
 
-        private bool CheckDirection(int row, int col, int dirRow, int dirCol, int playerValue)
+        private bool CheckDirection(int row, int col, int dirRow, int dirCol, int playerValue, List<Vector2Int> winningDiscs)
         {
             int count = 1;
+            winningDiscs.Clear();
 
-            count += CountConsecutiveDiscs(row, col, dirRow, dirCol, playerValue);
-            count += CountConsecutiveDiscs(row, col, -dirRow, -dirCol, playerValue);
+            count += CountConsecutiveDiscs(row, col, dirRow, dirCol, playerValue, winningDiscs);
+            count += CountConsecutiveDiscs(row, col, -dirRow, -dirCol, playerValue, winningDiscs);
 
             return count >= 4;
         }
 
-        private int CountConsecutiveDiscs(int row, int col, int dirRow, int dirCol, int playerValue)
+        private int CountConsecutiveDiscs(int row, int col, int dirRow, int dirCol, int playerValue, List<Vector2Int> winningDiscs)
         {
             int count = 0;
             int currentRow = row + dirRow;
             int currentColumn = col + dirCol;
-            int rowCount = GameManager.Instance.Get<BoardService>().RowCount;
-            int colCount = GameManager.Instance.Get<BoardService>().ColumnCount;
 
-            while (currentRow >= 0 && currentRow < rowCount && currentColumn >= 0 && currentColumn < colCount)
+            while (currentRow >= 0 && currentRow < _rowCount && currentColumn >= 0 && currentColumn < _colCount)
             {
                 if (GameManager.Instance.Get<BoardService>().GetBoardCellValue(currentRow, currentColumn) == playerValue)
                 {
                     count++;
+                    winningDiscs.Add(new Vector2Int(currentRow, currentColumn));
                     currentRow += dirRow;
                     currentColumn += dirCol;
                 }
@@ -102,9 +128,30 @@ namespace ConnectFourMultiplayer.Gameplay
             return count;
         }
 
+        private IEnumerator HighlightWinningDiscs(List<Vector2Int> winningDiscs)
+        {
+            yield return new WaitForSeconds(3f);
+
+            foreach (var discPos in winningDiscs)
+            {
+                Vector3 worldPosition = GetDiskPosition(discPos.x, discPos.y);
+                GameObject highlightedDisk = Instantiate(_highlightPrefab, worldPosition, Quaternion.identity);
+            }
+        }
+
         private void HandleWin()
         {
             Debug.Log("Player " + _playerTurn + " Won.");
+        }
+
+        public void InitializeSpawnedDiskMatrix(int rowCount, int colCount)
+        {
+            spawnedDisks = new GameObject[rowCount, colCount];
+        }
+
+        public Vector3 GetDiskPosition(int row, int col)
+        {
+            return spawnedDisks[row, col].gameObject.transform.position;
         }
     }
 }
