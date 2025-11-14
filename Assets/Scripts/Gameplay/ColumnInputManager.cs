37cede5775498a5093c0436f7834b174cd3d6755
Deependra-Das <iamdeep75@gmail.com>
2025-11-14 23:42:33 +0530

    Updated ColumnInputManager

--- Assets/Scripts/Gameplay/ColumnInputManager.cs
+++ Assets/Scripts/Gameplay/ColumnInputManager.cs
@@ -1,4 +1,5 @@
 using ConnectFourMultiplayer.Event;
+using ConnectFourMultiplayer.Network;
 using System.Collections;
 using UnityEngine;
 
@@ -9,66 +10,47 @@ namespace ConnectFourMultiplayer.Gameplay
         [SerializeField] private BoxCollider boxCollider;
         [SerializeField] private int _columnIndex;
         [SerializeField] private float _cooldownTime = 3f;
-        private bool _cooldownActive = false;
-        private bool _isGameOVer = false;
 
 
         private void OnEnable()
         {
-            EventBusManager.Instance.Subscribe(EventNameEnum.TakeTurn, HandleTakeTurnColumnInput);
+            EventBusManager.Instance.Subscribe(EventNameEnum.EnableColumnInput, HandleEnableColumnInput);
             EventBusManager.Instance.Subscribe(EventNameEnum.GameOver, HandleGameOverColumnInput);
         }
 
         private void OnDisable()
         {
-            EventBusManager.Instance.Unsubscribe(EventNameEnum.TakeTurn, HandleTakeTurnColumnInput);
+            EventBusManager.Instance.Unsubscribe(EventNameEnum.TakeTurn, HandleEnableColumnInput);
             EventBusManager.Instance.Unsubscribe(EventNameEnum.GameOver, HandleGameOverColumnInput);
         }
 
         private void OnMouseDown()
         {
-            if (!_cooldownActive)
-            {
-                _cooldownActive = true;
-                SetCollidersState(false);
-                GameplayManager.Instance.TakeTurn(_columnIndex);
-            }
+            bool turnSuccess = GameplayManager.Instance.TryTakeTurn(_columnIndex);
+            SetCollidersState(!turnSuccess);
         }
 
-        private void OnMouseOver()
+        //private void OnMouseOver()
+        //{
+        //    if (!_cooldownActive && !_isGameOVer)
+        //    {
+        //        GameplayManager.Instance.OnHoverOverColumn(_columnIndex);
+        //    }
+        //}
+
+        private void HandleEnableColumnInput(object[] parameters)
         {
-            if (!_cooldownActive && !_isGameOVer)
-            {
-                GameplayManager.Instance.OnHoverOverColumn(_columnIndex);
-            }
-        }
-
-        private void HandleTakeTurnColumnInput(object[] parameters)
-        {
-            StartCoroutine(CooldownForAllObjects(_cooldownTime));
-        }
-
-        private IEnumerator CooldownForAllObjects(float cooldownTime)
-        {
-            SetCollidersState(false);
-
-            yield return new WaitForSeconds(cooldownTime);
-
             SetCollidersState(true);
-
-            _cooldownActive = false;
         }
 
         private void SetCollidersState(bool state)
         {
-            {
-                GetComponent<Collider>().enabled = state;
-            }
+            boxCollider.enabled = state;
         }
 
         private void HandleGameOverColumnInput(object[] parameters)
         {
-            _isGameOVer = true;
+            SetCollidersState(false);
         }
     }
 }
