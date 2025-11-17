1b0f520951c3c7c70eee8e31598ef04b2281ca86
Deependra-Das <iamdeep75@gmail.com>
2025-11-17 18:42:45 +0530

    Updated GameOverUIManager to handle Disconnection

--- Assets/Scripts/UI/GameOver/GameOverUIManager.cs
+++ Assets/Scripts/UI/GameOver/GameOverUIManager.cs
@@ -17,6 +17,14 @@ namespace ConnectFourMultiplayer.UI
         [SerializeField] private GameObject _winnerDeclarationPanel;
         [SerializeField] private GameObject _drawDeclarationPanel;
 
+        [Header("Disconnected PopUp")]
+        [SerializeField] private GameObject _disconnectedPopUp;
+        [SerializeField] private TMP_Text _disconnectedCountdownCharSelectUIText;
+        [SerializeField] float _disconnectedCountdownTime = 5f;
+
+        private bool _isGameResultSet = false;
+        private bool _isCountDownOngoing = false;
+
         private void OnEnable() => SubscribeToEvents();
 
         private void OnDisable() => UnsubscribeToEvents();
@@ -25,17 +33,24 @@ namespace ConnectFourMultiplayer.UI
         {
             EventBusManager.Instance.Subscribe(EventNameEnum.SetWinnerOnGameOverUI, HandleWinnerDetailsOnGameOverUI);
             EventBusManager.Instance.Subscribe(EventNameEnum.StartGameOverCountdown, HandleStartGameOverCountdownUI);
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnect;
         }
 
         private void UnsubscribeToEvents()
         {
             EventBusManager.Instance.Unsubscribe(EventNameEnum.SetWinnerOnGameOverUI, HandleWinnerDetailsOnGameOverUI);
             EventBusManager.Instance.Unsubscribe(EventNameEnum.StartGameOverCountdown, HandleStartGameOverCountdownUI);
+            
+            if (NetworkManager.Singleton != null)
+            {
+                NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnect;
+            }
         }
 
         private void HandleWinnerDetailsOnGameOverUI(object[] parameters)
         {
             SetGameResult();
+            _isGameResultSet = true;
             EnableView();
         }
 
@@ -86,6 +101,7 @@ namespace ConnectFourMultiplayer.UI
 
         private IEnumerator GameOverCountdownSequence()
         {
+            _isCountDownOngoing = true;
             float currentTime = _gameOverCountdownValue;
 
             while (currentTime > 0)
@@ -103,6 +119,50 @@ namespace ConnectFourMultiplayer.UI
         {
             GameManager.Instance.Get<CleanUpService>().CleanUpMultiplayerManager();
             GameManager.Instance.Get<CleanUpService>().CleanUpNetworkManager();
+        }
+
+        private void HandleClientDisconnect(ulong clientId)
+        {
+            if (_isGameResultSet)
+            {
+                if(!_isCountDownOngoing)
+                {
+                    StartCoroutine(GameOverCountdownSequence());
+                }
+            }
+            else
+            {
+                GameManager.Instance.Get<CleanUpService>().CleanUpPlayerSessionDataManager();
+                GameManager.Instance.Get<CleanUpService>().CleanUpLobbyRelay();
+                GameManager.Instance.Get<CleanUpService>().ResetServices();
+                ShowDisconnectedNotification();
+                StartCoroutine(DisconnectedCountdownSequence());
+            }
+
+        }
+        private void ShowDisconnectedNotification()
+        {
+            _disconnectedPopUp.SetActive(true);
+        }
+
+        private IEnumerator DisconnectedCountdownSequence()
+        {
+            float currentTime = _disconnectedCountdownTime;
+
+            while (currentTime > 0)
+            {
+                _disconnectedCountdownCharSelectUIText.text = "Returning To Main Menu In... " + Mathf.Ceil(currentTime).ToString() + "s";
+                currentTime -= 1f;
+                yield return new WaitForSeconds(1f);
+            }
+
+            HideDisconnectedNotification();
+            SceneLoader.Instance.LoadScene(SceneNameEnum.MainMenuScene, false);
+        }
+
+        private void HideDisconnectedNotification()
+        {
+            _disconnectedPopUp.SetActive(false);
         }
     }
 }
